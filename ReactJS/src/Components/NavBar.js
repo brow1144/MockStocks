891c3bada90d7afc64c1a7837b7a0c89c88be0a5
Kyle Brown <brow1144@purdue.edu>
2018-09-09 20:35:23 -0400

    NavBar UI set up / started implementing highcharts

--- ReactJS/src/Components/NavBar.js
+++ ReactJS/src/Components/NavBar.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
 
+import {FormInline, Fa} from 'mdbreact';
+import {Row, Col} from 'reactstrap';
+
+import '../Static/CSS/NavBar.css';
+
 class NavBar extends Component {
   render() {
 
     // Finally, render it!
     return (
-      <p>NavBar</p>
+      <div>
+        <Row>
+          <Col className='title' sm='2'>
+            <b>Portfol.io</b>
+          </Col>
+          <Col className='blackBack' sm='6'>
+            <FormInline className="md-form">
+              <Fa style={{color: 'whitesmoke'}} icon="search" />
+              <input style={{color: 'whitesmoke'}} className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search"/>
+            </FormInline>
+          </Col>
+          <Col className='navText' sm='2'>
+            <b>Home</b>
+          </Col>
+          <Col className='navText' sm='2'>
+            <b>Games</b>
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
--- ReactJS/src/Pages/Home.js
+++ ReactJS/src/Pages/Home.js
@@ -2,12 +2,38 @@ import React, {Component} from 'react';
 
 import NavBar from '../Components/NavBar';
 
+import Highcharts from 'highcharts/highstock'
+import HighchartsReact from 'highcharts-react-official'
+
+import {Row, Col} from 'reactstrap';
+
+import '../../node_modules/highcharts/css/themes/dark-unica.css';
+
 class Home extends Component {
 
   render() {
+  // Apply the theme
+  // Highcharts.Highcharts.setOptions(HighchartsTheme.dark);
+    const options = {
+      title: {
+        text: 'My stock chart'
+      },
+      series: [{
+        data: [1, 2, 3]
+      }]
+    }
+    
+
     return (
       <div>
         <NavBar />
+
+        {/* <HighchartsReact
+          highcharts={Highcharts}
+          constructorType={'stockChart'}
+          options={options}
+        /> */}
+        
       </div>
     );
   }
