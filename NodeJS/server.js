2e2168f55a5cd7fd1dd385f2a9bfdae6e4cc3a8c
Jake Koontz <jmkoontz@purdue.edu>
2018-09-11 18:34:35 -0400

    Initial server setup

---
+++ NodeJS/server.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let mongoose = require('mongoose');
+
+let app = express();
+
+// connect to the database
+
+// listen to port
+app.listen(3000);
+console.log("Now listening on port 3000.");
