11a39e1514e2ba68e6a6c8e8e9fa293f2d144a46
Riley Robertson <rober343@purdue.edu>
2018-09-13 20:24:07 -0400

    File Setup

---
+++ ReactJS/src/Components/UpdateGame.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+import '../Static/CSS/UpdateGame.css'
+
+class UpdateGame extends Component {
+
+    render() {
+        return (
+            <div>
+                List of Games
+            </div>
+        );
+    }
+}
+
+export default UpdateGame;
