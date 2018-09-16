da38d1b7b287d622710ce03d3ddb1728f618fd9e
Walter Jacquette <wjacquet@purdue.edu>
2018-09-16 15:29:43 -0400

    Installed Babel and uses ES6 imports also changed to port 8080

--- NodeJS/Controllers/controller.js
+++ NodeJS/Controllers/controller.js
@@ -1,9 +1,9 @@
-let bodyParser = require('body-parser');
-let model = require('../Models/testMongo'); // must import models that we need
+import bodyParser from 'body-parser';
+import {getUsers} from '../Models/testMongo'; // must import models that we need
 
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 
-module.exports = (app) => {
+export default (app) => {
   // heres the endpoints obviously
   app.get('/Portfol.io/Home', async (req, res) => {
     // get data from mongodb and pass it to view
@@ -15,7 +15,7 @@ module.exports = (app) => {
 
     // Method #3 - async/await
     // async await looks slick so ive been doing it but method 2 is just as valid
-    let data = await model.getUsers();
+    let data = await getUsers();
     console.log("got it: " + data);
     res.status(200).json(data);
   });
