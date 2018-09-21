94e6b9d1564f94d7122ef3136915ba33b8462622
Riley Robertson <rober343@purdue.edu>
2018-09-21 15:02:30 -0400

    Games Page formatting

---
+++ ReactJS/src/Components/Games/GamesList.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import { Table } from 'reactstrap'
+
+
+class GameList extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div  className='z-depth-5' >
+                <h5 className={"gamesText"}>Floor List</h5>
+                <Table dark>
+                    <thead>
+                    <tr>
+                        <th>Floor Name</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">xxN0Sc0p35xx</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Sperg Floor</th>
+                    </tr>
+                    <tr>
+                        <th scope="row">Shrek's Swamp</th>
+                    </tr>
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
+}
+
+export default GameList;
