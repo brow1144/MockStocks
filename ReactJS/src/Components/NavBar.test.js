c91fa84aa7788a0e6a32820a40f430ebe4442576
Kyle Brown <brow1144@purdue.edu>
2018-09-25 17:34:47 -0400

    Basic search complete

--- ReactJS/src/Components/NavBar.test.js
+++ ReactJS/src/Components/NavBar.test.js
@@ -3,9 +3,11 @@ import {shallow} from 'enzyme';
 import NavBar from './NavBar';
 import axios from 'axios';
 
-const noResults = [{}];
-const oneResult = [{ticker: 'AAPL', company: 'Apple'}];
-
+const tickers = [{symbol: 'AAPL', company: 'Apple Inc.'}, {symbol: 'BCEI', company: 'Bonzana Creek Energy'}, {symbol: 'GOOGL', company: 'Alphabet'},
+                 {symbol: 'ACER', company: 'ACER Theraputics'}, {symbol: 'CEI', company: 'Camber Energy'}, {symbol: '', company: ''},
+                 {symbol: 'ACES', company: 'ALPS Clean Energy'}, {symbol: 'ACET', company: 'Aceto Corporation'}, {symbol: 'CEIX', company: 'CONSOL Energy'},
+                 {symbol: 'BCE', company: 'BCE Inc'}, {symbol: 'CCE', company: 'Coke European Partners'}]
+              
 const then = jest.fn(() => {
   return {catch: jest.fn()};
 })
@@ -16,19 +18,23 @@ axios.get = jest.fn((url) => {
 
 describe('Positive Search Results', () => {
 
-  // test('Search result has >= 6 results', () => {
-  //   // TODO
-  //   expect(true).toBe(true);
-  // })
+  test('Search result has >= 6 results', () => {
+    const navBar = shallow(<NavBar />);
+    navBar.setState({tickers: tickers})
+    navBar.find('#search').simulate('change', {target: {value: 'C'}});
+    expect(navBar.state().tickersShowing.length).toBe(6)  
+  })
 
-  // TODO 
-  // test('Search result has < 6 results', () => {
-    
-  // })
+  test('Search result has < 6 results', () => {
+    const navBar = shallow(<NavBar />);
+    navBar.setState({tickers: tickers})
+    navBar.find('#search').simulate('change', {target: {value: 'CEI'}});
+    expect(navBar.state().tickersShowing.length).toBe(3)
+  })
 
-  // TODO
   test('Search result has 0 results', () => {
     const navBar = shallow(<NavBar />);
+    navBar.setState({tickers: tickers})
     navBar.find('#search').simulate('change', {target: {value: 'CS408 is awesome'}});
     expect(navBar.state().tickersShowing.length).toBe(0);
   })
