26bb597785d8852f7de566072bd5573d085ff015
Jake Koontz <jmkoontz@purdue.edu>
2018-10-11 13:21:23 -0400

    uncommenting

--- NodeJS/Controllers/historyController.js
+++ NodeJS/Controllers/historyController.js
@@ -2,25 +2,34 @@ import schedule from 'node-schedule';
 import {getAllUsers} from '../Models/userDAO';
 
 export async function runSchedulers() {
-  // try {
-  //   let allUsers = await getAllUsers();
-  //
-  //   for (let i in allUsers) {
-  //     if (allUsers.hasOwnProperty(i)) {
-  //       let user = allUsers[i].toObject();
-  //       console.log(user._id);
-  //
-  //       for (let j in user.active_games) {
-  //         if (user.active_games.hasOwnProperty(j)) {
-  //           console.log(user.active_games[j]);
-  //         }
-  //       }
-  //     }
-  //   }
-  //
-  // } catch (error) {
-  //   console.log(error);
-  // }
+  let ownedStocks = [];
+
+  try {
+    let allUsers = await getAllUsers();
+
+    for (let i in allUsers) {
+      if (allUsers.hasOwnProperty(i)) {
+        let user = allUsers[i].toObject();
+        console.log(user._id);
+
+        for (let j in user.active_games) {
+          if (user.active_games.hasOwnProperty(j)) {
+            let game = user.active_games[j];
+            console.log(game);
+
+            for (let k in game.stocks) {
+              if (game.stocks.hasOwnProperty(k)) {
+                console.log(game.stocks[k]);
+              }
+            }
+          }
+        }
+      }
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
 
   // update portofolio values every weekday at 9:30 am
   let marketOpen = schedule.scheduleJob('30 09 * * 1-5', async () => {
