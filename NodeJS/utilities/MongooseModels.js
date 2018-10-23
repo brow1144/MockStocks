a8df4fadd09efb34f85755f39acde6221c54ea10
Kyle Brown <brow1144@purdue.edu>
2018-10-23 09:05:01 -0400

    Updates

--- NodeJS/utilities/MongooseModels.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-import {gameSchema, userSchema, tickerSchema} from './MongooseSchemas';
-
-export const gameModel = mongoose.model('Game', gameSchema);
-export const userModel = mongoose.model('User', userSchema);
-export const tickerModel = mongoose.model('Ticker', tickerSchema);
