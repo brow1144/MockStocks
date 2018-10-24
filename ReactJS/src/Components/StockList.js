163b2505a39104be8104ed258459d4e0e6be69f4
Eric Thiem <ericthiem23@gmail.com>
2018-10-24 16:13:19 -0700

    fixing Watchlist link but breaking StockList.test

--- ReactJS/src/Components/StockList.js
+++ ReactJS/src/Components/StockList.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react'
 import { Card, CardBody, CardTitle } from 'mdbreact'
-import { NavLink, BrowserRouter} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import StockCard from '../Components/StockCard'
 
 import '../Static/CSS/StockList.css'
 
 class StockList extends Component {
-
-  componentWillMount(){
-    this.getTopRisers();
-  }
 
   getTopRisers(){
     let watchlist = this.props.watchlist;
@@ -29,7 +25,7 @@ class StockList extends Component {
         <Card style={{backgroundColor: '#1B1B1D', color: 'whitesmoke'}}>
           <CardBody>
               <div className="article-container-inner">
-                <CardTitle stlyle={{borderBottom: '2px solid whitesmoke'}} >Watchlist Top Risers</CardTitle>
+                <CardTitle style={{borderBottom: '2px solid whitesmoke'}} >Watchlist Top Risers</CardTitle>
 
               </div>
             {this.getTopRisers().map((stock, key) => {
@@ -44,11 +40,9 @@ class StockList extends Component {
               )
             })}
             <hr className='hr'/>
-            <BrowserRouter>
-              <NavLink to={'/Portfolio/Watchlist'} style={{textDecoration: 'none'}}>
-                <b className='navText' style={{fontSize: '1em'}}>View Full Watchlist</b>
-              </NavLink>
-            </BrowserRouter>
+            <NavLink to={'/Portfolio/Watchlist'} style={{textDecoration: 'none'}}>
+              <b className='navText' style={{fontSize: '1em'}}>View Full Watchlist</b>
+            </NavLink>
           </CardBody>
         </Card>
       </div>
